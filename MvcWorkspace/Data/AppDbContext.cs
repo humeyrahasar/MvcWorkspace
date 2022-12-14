9e734b1e40ca8e162084a1b0d4e5007e00971071
HUMEYRA HASAR <humeyra.hasar@gmail.com>
2022-12-14 14:34:05 +0300

    Proje Setup with EF, cleanup

---
+++ MvcWorkspace/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MvcWorkspace.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+    }
+}
