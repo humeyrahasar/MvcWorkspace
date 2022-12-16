b5acd50a4df12d35ec047e61d54729acb42ed2e9
HUMEYRA HASAR <humeyra.hasar@gmail.com>
2022-12-16 13:28:13 +0300

    model,migration,updateDb tamamlandı

--- MvcWorkspace/Data/AppDbContext.cs
+++ MvcWorkspace/Data/AppDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MvcWorkspace.Models;
 
 namespace MvcWorkspace.Data
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions options) : base(options) 
+        public AppDbContext(DbContextOptions options) : base(options)
         {
         }
+        public DbSet<Expense> Expenses { get; set; }
     }
 }
---
+++ MvcWorkspace/Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcWorkspace.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name= "Expense Name")]
+        public string ExpenceName{ get; set; }
+
+        [Required]
+        [Range(1,int.MaxValue, ErrorMessage ="Yanlış Değer")]
+        public int Amount { get; set; }
+    }
+}
